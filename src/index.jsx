50017557b78d03ad87ceeccf01da43c9a852b396
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-09-10 13:13:07 +0300

    solve main.js problem 😊

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min'; 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../public/style.css';
-import Header from './components/Header';
+import { init, resizeReset, mousemove, mouseout } from './main';  
+import Header from './components/Header.jsx';
 
+function Root() {
+  const canvasRef = useRef(null);  
+  useEffect(() => {
+    const canvas = canvasRef.current;  
+    init(canvas);  
+
+    window.addEventListener("resize", () => resizeReset(canvas)); 
+    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mouseout", mouseout);
+    
+    return () => {
+      window.removeEventListener("resize", () => resizeReset(canvas));
+      window.removeEventListener("mousemove", mousemove);
+      window.removeEventListener("mouseout", mouseout);
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <Header />
+      <App />
+      <canvas ref={canvasRef} id="canvas"></canvas> 
+      {/* <Footer /> */}
+    </React.StrictMode>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Header />
-     <App />
-    {/* <Footer />  */}
-  </React.StrictMode>,
+  <Root />,
   document.getElementById('root')
 );
